d5066851ff58099f12844d0fb7cbb53f9b095805
FelipeUCC898 <felipe.narvaezg@campusucc.edu.co>
2025-11-23 01:41:09 -0500

    Proyecto 100% funcional

---
+++ src/main/java/com/labturnos/LabTurnosApplication.java
@@ -0,0 +1,13 @@
+package com.labturnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTurnosApplication.class, args);
+	}
+
+}
