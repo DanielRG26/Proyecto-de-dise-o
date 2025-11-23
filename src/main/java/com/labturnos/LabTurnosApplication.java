e3e715af874326c21ecc1de2d97ffbf64e713e2b
DanielRG26 <daniel.guevararodr@campusucc.edu.co>
2025-11-23 12:24:14 -0500

    Subir sistema de gestión de turnos: backend y frontend actualizados

--- src/main/java/com/labturnos/LabTurnosApplication.java
+++ src/main/java/com/labturnos/LabTurnosApplication.java
@@ -2,8 +2,10 @@ package com.labturnos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LabTurnosApplication {
 
 	public static void main(String[] args) {
